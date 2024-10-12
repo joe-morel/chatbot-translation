58ae5d17d648bd82e8201a43bb5d7879df260898
“Joe <joemorel06@hotmail.com>
2024-10-12 16:27:38 -0300

    Adding loading state with setTimeout of 500ms

--- src/components/CardsChat.tsx
+++ src/components/CardsChat.tsx
@@ -1,22 +1,17 @@
 "use client";
 
-import * as React from "react"
-import { Check, Plus, Send } from "lucide-react"
+import * as React from "react";
+import { Check, Plus, Send, Loader2 } from "lucide-react"; // Asegúrate de importar Loader2
 
-
-import { cn } from "@/lib/utils"
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "../components/ui/avatar"
-import { Button } from "../components/ui/button"
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { Button } from "../components/ui/button";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
-} from "../components/ui/card"
+} from "../components/ui/card";
 import {
   Command,
   CommandEmpty,
@@ -24,7 +19,7 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "../components/ui/command"
+} from "../components/ui/command";
 import {
   Dialog,
   DialogContent,
@@ -32,17 +27,16 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "../components/ui/dialog"
-import { Input } from "../components/ui/input"
+} from "../components/ui/dialog";
+import { Input } from "../components/ui/input";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "../components/ui//tooltip"
+} from "../components/ui/tooltip";
 
-const TARGET_LANGUAGE = "en"; 
-
+const TARGET_LANGUAGE = "en";
 
 const users = [
   {
@@ -70,73 +64,61 @@ const users = [
     email: "will@email.com",
     avatar: "/avatars/04.png",
   },
-] as const
+] as const;
 
-type User = (typeof users)[number]
-
+type User = (typeof users)[number];
 
 export default function CardsChat() {
-    const [open, setOpen] = React.useState(false)
-    const [selectedUsers, setSelectedUsers] = React.useState<User[]>([])
+  const [open, setOpen] = React.useState(false);
+  const [selectedUsers, setSelectedUsers] = React.useState<User[]>([]);
+  const [messages, setMessages] = React.useState([
+    {
+      role: "agent",
+      content: "Hello, write me something and I will help you to translate it.",
+    },
+  ]);
+  const [input, setInput] = React.useState("");
+  const [loading, setLoading] = React.useState(false); // Estado de loading
+  const inputLength = input.trim().length;
 
-
-    
-  
-    const [messages, setMessages] = React.useState([
-      {
-        role: "agent",
-        content: "Hello, write me something and I will help you to translate it.",
-      },
-      // {
-      //   role: "user",
-      //   content: "Hey, I'm having trouble with my account.",
-      // },
-      // {
-      //   role: "agent",
-      //   content: "What seems to be the problem?",
-      // },
-      // {
-      //   role: "user",
-      //   content: "I can't log in.",
-      // },
-    ])
-    const [input, setInput] = React.useState("")
-    const inputLength = input.trim().length
-
-    const translateMessage = async (message: string) => {
-      try {
-        const response = await fetch(
-          `https://translation.googleapis.com/language/translate/v2?key=AIzaSyBTGnW1qen-dW1x8q332rrLjKeF5nB57Js`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              q: message,
-              target: TARGET_LANGUAGE,
-            }),
-          }
-        );
-    
-        if (!response.ok) {
-          throw new Error("Error en la traducción");
+  const translateMessage = async (message: string) => {
+    setLoading(true); // Iniciar loading
+    try {
+           // Simular un tiempo de espera mínimo (por ejemplo, 500 ms)
+           await new Promise(resolve => setTimeout(resolve, 500));
+      const response = await fetch(
+        `https://translation.googleapis.com/language/translate/v2?key=AIzaSyBTGnW1qen-dW1x8q332rrLjKeF5nB57Js`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            q: message,
+            target: TARGET_LANGUAGE,
+          }),
         }
-    
-        const data = await response.json();
-        return data.data.translations[0].translatedText;
-      } catch (error) {
-        console.error("Error traduciendo el mensaje:", error);
-        return message; // Si hay un error, devuelve el mensaje original
+      );
+
+      if (!response.ok) {
+        throw new Error("Error en la traducción");
       }
-    };
-    
 
-    const handleSendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      if (inputLength === 0) return;
+      const data = await response.json();
+      return data.data.translations[0].translatedText;
+    } catch (error) {
+      console.error("Error traduciendo el mensaje:", error);
+      return message; // Si hay un error, devuelve el mensaje original
+    } finally {
+      setLoading(false); // Detener loading
+    }
+  };
 
-      // Agregar el mensaje original del usuario al estado de mensajes
+  const handleSendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputLength === 0) return;
+
+    // Agregar el mensaje original del usuario al estado de mensajes
     setMessages((prevMessages) => [
       ...prevMessages,
       {
@@ -145,111 +127,81 @@ export default function CardsChat() {
       },
     ]);
 
-      // const translatedMessage = await translateMessage(input); // Traduce el mensaje
-      // setMessages([
-      //   ...messages,
-      //   {
-      //     role: "user",
-      //     content: translatedMessage,
-      //   },
-      // ]);
-      // Traducir el mensaje
+    // Traducir el mensaje
     const translatedMessage = await translateMessage(input);
-     
-     // Agregar la traducción como un nuevo mensaje del agente
-     setMessages((prevMessages) => [
-       ...prevMessages,
-       {
-         role: "agent",
-         content: translatedMessage,
-       },
-     ]);
 
-      setInput("");
-    };
-  
-    return (
-      <>
-        <Card>
-          <CardHeader className="flex flex-row items-center">
-            <div className="flex items-center space-x-4">
-              <Avatar>
-                <AvatarImage src="/avatars/01.png" alt="Image" />
-                <AvatarFallback>OM</AvatarFallback>
-              </Avatar>
-              <div>
-                <p className="text-sm font-medium leading-none">Sofia Davis</p>
-                <p className="text-sm text-muted-foreground">m@example.com</p>
-              </div>
+    // Agregar la traducción como un nuevo mensaje del agente
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        role: "agent",
+        content: translatedMessage,
+      },
+    ]);
+
+    setInput("");
+  };
+
+  return (
+    <>
+      <Card>
+        <CardHeader className="flex flex-row items-center">
+          <div className="flex items-center space-x-4">
+            <Avatar>
+              <AvatarImage src="/avatars/01.png" alt="Image" />
+              <AvatarFallback>OM</AvatarFallback>
+            </Avatar>
+            <div>
+              <p className="text-sm font-medium leading-none">Sofia Davis</p>
+              <p className="text-sm text-muted-foreground">m@example.com</p>
             </div>
-            <TooltipProvider delayDuration={0}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    size="icon"
-                    variant="outline"
-                    className="ml-auto rounded-full"
-                    onClick={() => setOpen(true)}
-                  >
-                    <Plus className="h-4 w-4" />
-                    <span className="sr-only">New message</span>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent sideOffset={10}>New message</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </CardHeader>
-          <CardContent>
-            <div className="space-y-4">
-              {messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={cn(
-                    "flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
-                    message.role === "user"
-                      ? "ml-auto bg-primary text-primary-foreground"
-                      : "bg-muted"
-                  )}
+          </div>
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  size="icon"
+                  variant="outline"
+                  className="ml-auto rounded-full"
+                  onClick={() => setOpen(true)}
                 >
-                  {message.content}
-                </div>
-              ))}
-            </div>
-          </CardContent>
-          {/* <CardFooter>
-            <form
-              onSubmit={(event) => {
-                event.preventDefault()
-                if (inputLength === 0) return
-                setMessages([
-                  ...messages,
-                  {
-                    role: "user",
-                    content: input,
-                  },
-                ])
-                setInput("")
-              }}
-              className="flex w-full items-center space-x-2"
-            >
-              <Input
-                id="message"
-                placeholder="Type your message..."
-                className="flex-1"
-                autoComplete="off"
-                value={input}
-                onChange={(event) => setInput(event.target.value)}
-              />
-              <Button type="submit" size="icon" disabled={inputLength === 0}>
-                <Send className="h-4 w-4" />
-                <span className="sr-only">Send</span>
-              </Button>
-            </form>
-          </CardFooter> */}
+                  <Plus className="h-4 w-4" />
+                  <span className="sr-only">New message</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent sideOffset={10}>New message</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={cn(
+                  "flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
+                  message.role === "user"
+                    ? "ml-auto bg-primary text-primary-foreground"
+                    : "bg-muted"
+                )}
+              >
+                {message.content}
+              </div>
+            ))}
+            {loading && ( // Mostrar el spinner si loading es true
+              <div className="flex items-center">
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                <span>Translating...</span>
+              </div>
+            )}
+          </div>
+        </CardContent>
 
-
-<CardFooter>
-          <form onSubmit={handleSendMessage} className="flex w-full items-center space-x-2">
+        <CardFooter>
+          <form
+            onSubmit={handleSendMessage}
+            className="flex w-full items-center space-x-2"
+          >
             <Input
               id="message"
               placeholder="Type your message..."
@@ -258,96 +210,98 @@ export default function CardsChat() {
               value={input}
               onChange={(event) => setInput(event.target.value)}
             />
-            <Button type="submit" size="icon" disabled={inputLength === 0}>
+            <Button
+              type="submit"
+              size="icon"
+              disabled={inputLength === 0 || loading}
+            >
               <Send className="h-4 w-4" />
               <span className="sr-only">Send</span>
             </Button>
           </form>
         </CardFooter>
-        </Card>
-        <Dialog open={open} onOpenChange={setOpen}>
-          <DialogContent className="gap-0 p-0 outline-none">
-            <DialogHeader className="px-4 pb-4 pt-5">
-              <DialogTitle>New message</DialogTitle>
-              <DialogDescription>
-                Invite a user to this thread. This will create a new group
-                message.
-              </DialogDescription>
-            </DialogHeader>
-            <Command className="overflow-hidden rounded-t-none border-t">
-              <CommandInput placeholder="Search user..." />
-              <CommandList>
-                <CommandEmpty>No users found.</CommandEmpty>
-                <CommandGroup className="p-2">
-                  {users.map((user) => (
-                    <CommandItem
-                      key={user.email}
-                      className="flex items-center px-2"
-                      onSelect={() => {
-                        if (selectedUsers.includes(user)) {
-                          return setSelectedUsers(
-                            selectedUsers.filter(
-                              (selectedUser) => selectedUser !== user
-                            )
-                          )
-                        }
-  
+      </Card>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="gap-0 p-0 outline-none">
+          <DialogHeader className="px-4 pb-4 pt-5">
+            <DialogTitle>New message</DialogTitle>
+            <DialogDescription>
+              Invite a user to this thread. This will create a new group
+              message.
+            </DialogDescription>
+          </DialogHeader>
+          <Command className="overflow-hidden rounded-t-none border-t">
+            <CommandInput placeholder="Search user..." />
+            <CommandList>
+              <CommandEmpty>No users found.</CommandEmpty>
+              <CommandGroup className="p-2">
+                {users.map((user) => (
+                  <CommandItem
+                    key={user.email}
+                    className="flex items-center px-2"
+                    onSelect={() => {
+                      if (selectedUsers.includes(user)) {
                         return setSelectedUsers(
-                          [...users].filter((u) =>
-                            [...selectedUsers, user].includes(u)
+                          selectedUsers.filter(
+                            (selectedUser) => selectedUser !== user
                           )
+                        );
+                      }
+
+                      return setSelectedUsers(
+                        [...users].filter((u) =>
+                          [...selectedUsers, user].includes(u)
                         )
-                      }}
-                    >
-                      <Avatar>
-                        <AvatarImage src={user.avatar} alt="Image" />
-                        <AvatarFallback>{user.name[0]}</AvatarFallback>
-                      </Avatar>
-                      <div className="ml-2">
-                        <p className="text-sm font-medium leading-none">
-                          {user.name}
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          {user.email}
-                        </p>
-                      </div>
-                      {selectedUsers.includes(user) ? (
-                        <Check className="ml-auto flex h-5 w-5 text-primary" />
-                      ) : null}
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-            <DialogFooter className="flex items-center border-t p-4 sm:justify-between">
-              {selectedUsers.length > 0 ? (
-                <div className="flex -space-x-2 overflow-hidden">
-                  {selectedUsers.map((user) => (
-                    <Avatar
-                      key={user.email}
-                      className="inline-block border-2 border-background"
-                    >
-                      <AvatarImage src={user.avatar} />
+                      );
+                    }}
+                  >
+                    <Avatar>
+                      <AvatarImage src={user.avatar} alt="Image" />
                       <AvatarFallback>{user.name[0]}</AvatarFallback>
                     </Avatar>
-                  ))}
-                </div>
-              ) : (
-                <p className="text-sm text-muted-foreground">
-                  Select users to add to this thread.
-                </p>
-              )}
-              <Button
-                disabled={selectedUsers.length < 2}
-                onClick={() => {
-                  setOpen(false)
-                }}
-              >
-                Continue
+                    <div className="ml-2">
+                      <p className="text-sm font-medium leading-none">
+                        {user.name}
+                      </p>
+                      <p className="text-sm text-muted-foreground">
+                        {user.email}
+                      </p>
+                    </div>
+                    {selectedUsers.includes(user) ? (
+                      <Check className="ml-auto flex h-5 w-5 text-primary" />
+                    ) : null}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+          <DialogFooter className="flex items-center border-t p-4 sm:justify-between">
+            {selectedUsers.length > 0 ? (
+              <div className="flex -space-x-2 overflow-hidden">
+                {selectedUsers.map((user) => (
+                  <Avatar
+                    key={user.email}
+                    className="inline-block border-2 border-background"
+                  >
+                    <AvatarImage src={user.avatar} />
+                    <AvatarFallback>{user.name[0]}</AvatarFallback>
+                  </Avatar>
+                ))}
+              </div>
+            ) : (
+              <p className="text-sm text-muted-foreground">
+                Select users to add to this thread.
+              </p>
+            )}
+            <DialogFooter>
+              <Button variant="outline" onClick={() => setOpen(false)}>
+                Cancel
               </Button>
+              <Button onClick={() => setOpen(false)}>Create</Button>
             </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </>
-    )
-  }
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
